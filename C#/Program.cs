0f4d708e5760d801fc7e851807d13a16b29c5707
Zeros遇见-Sicont <zeros_zhang@163.com>
2023-04-21 15:36:03 +0800

    更新

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,3 +1,4 @@
+using Internal;
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
@@ -7,11 +8,8 @@ class SimpleProgram
 {
     static void Main()
     {
-        CancellationTokenSource mTokenSource = new CancellationTokenSource();
-        mTokenSource.Token.Register(() => Console.WriteLine("我被取消了."));
-
-        mTokenSource.CancelAfter(5000);
-        Console.WriteLine("不会阻塞,我会执行.");
+        int i = 128;
+        Console.WriteLine($"测试{i:0x}");
     }
 }
 
