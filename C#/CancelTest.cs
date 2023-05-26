dce5c5663ab4999cfe31bf84c19520b351a8c5f9
Zeros遇见-Sicont <zeros_zhang@163.com>
2023-05-26 13:03:08 +0800

    更新

--- C#/CancelTest.cs
+++
@@ -1,9 +0,0 @@
-CancellationTokenSource mTokenSource1 = new CancellationTokenSource();
-
-CancellationTokenSource mTokenSource2 = new CancellationTokenSource();
-mTokenSource2.Token.Register(() => System.Console.WriteLine("mTokenSource2被取消了"));
-
-CancellationTokenSource mTokenSourceNew = CancellationTokenSource.CreateLinkedTokenSource(mTokenSource1.Token, mTokenSource2.Token);
-mTokenSourceNew.Token.Register(() => System.Console.WriteLine("mTokenSourceNew被取消了"));
-
-mTokenSource1.Cancel();
